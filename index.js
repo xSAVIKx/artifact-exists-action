5c4e3cd82e0d9f97ebae4602e02d3aa6448d7413
Yurii Serhiichuk <savik.ne@gmail.com>
2024-01-02 19:17:07 +0100

    Do migrate to artifact v2 API

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 const core = require("@actions/core");
-const artifact = require("@actions/artifact");
+const artifactClient = require("@actions/artifact");
 const fs = require("fs");
 
 async function run() {
   try {
     const name = core.getInput("name", { required: true });
     const tempArtifactPath = fs.mkdtempSync("artifactExists") + `/${name}`;
-    const artifactClient = artifact.create();
     // download a single artifact
     core.info(
-      `Starting download for ${name}. Temporary storing at ${tempArtifactPath} if exists.`
+      `Starting download for ${name}. Temporary storing at ${tempArtifactPath} if exists.`,
     );
-    const downloadOptions = {
-      createArtifactFolder: false,
-    };
     try {
-      const downloadResponse = await artifactClient.downloadArtifact(
-        name,
-        tempArtifactPath,
-        downloadOptions
+      const downloadResponse = await artifactClient.getArtifact(name);
+      core.info(
+        `Artifact ${downloadResponse.artifact.name} exists and has ID ${downloadResponse.artifact.id}.`,
       );
-      core.info(`Artifact ${downloadResponse.artifactName} exists.`);
       core.setOutput("exists", true);
     } catch (err) {
       core.info(`Artifact ${name} does not exist.`);
